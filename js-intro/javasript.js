e3df6489301d6c5f8e078878c97fdf5db41163e5
Mahith R <mahithr16@gmail.com>
2022-10-21 10:06:52 +0530

    JS-fundamentals-i

---
+++ js-intro/javasript.js
@@ -0,0 +1,15 @@
+console.log("Hello World!");
+
+let myNumber = 2;
+let txt = "";
+
+while (myNumber != Infinity) {
+    myNumber = myNumber * myNumber;
+    txt = txt + myNumber + "<br>";
+}
+
+console.log (txt);
+
+let a = 2;
+let x = 1 + (a *=2);
+console.log (x);
